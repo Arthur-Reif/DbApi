4095a5d37cd1483d7a5fcd2207624728d367aabe
Arthur-Reif <rreifarthur@gmail.com>
2026-09-03 19:33:29 -0300

    required adicionado

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Security.AccessControl;
 
 namespace DbApi.Models
@@ -5,10 +6,11 @@ namespace DbApi.Models
     public class Cliente
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required] //esta se referindo a nome
         public string Nome { get; set; }
 
         public string Telefone { get; set; }
-
+        [Required] //esta se referindo a email
         public string Email { get; set; }
 
         public string Endereco { get; set; }
